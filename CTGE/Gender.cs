2391830ce0476246169ec3da67b803078eb30fd6
Canyon Mooney <9002449+noy-nac@users.noreply.github.com>
2020-11-06 10:21:45 -0600

    Add files via upload

---
+++ CTGE/Gender.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTGE {
+
+    public enum GENDER_ID : sbyte {
+        Error = -128,
+        Empty = -1,
+        None = 0,
+        Male,
+        Female,
+        Other,
+        Not_Specified,
+        Boy,
+        Girl,
+        Man,
+        Woman,
+        Agender,
+        Agenderflux,
+        Androgyne,
+        Aporagender,
+        Bigender,
+        Butch,
+        Demiboy,
+        Demigender,
+        Demigirl,
+        Enby,
+        Femme,
+        Female_To_X,
+        Genderfluid,
+        Genderflux,
+        Genderless,
+        Gender_Neutral,
+        Genderqueer,
+        Gendervoid,
+        Intergender,
+        Maverique,
+        Male_To_X,
+        Neutrois,
+        Nonbinary,
+        Polygender,
+        Queer,
+        Trans_Feminine,
+        Transgender,
+        Vaguegender,
+        X_To_X,
+    }
+
+    public struct Gender {
+        public GENDER_ID id;
+    }
+}
---
+++ CTGE/Homeland.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTGE {
+
+    public enum HOMELAND_ID : sbyte {
+        Error = -128,
+        Empty = -1,
+        None = 0,
+        Ingens_Cistalia,
+        Ied_Kundetia,
+        Quod_Sacrosanct,
+        Cerkeran,
+        Sid_Praternit,
+        Zum_Tregaron,
+        Nihilavera,
+        Nan_Harapext,
+        Kinslaunth,
+        Grav_Septres,
+        Loukuttens,
+        Enam_Isacarathe,
+    }
+
+    public struct Homeland {
+        public HOMELAND_ID id;
+    }
+}
